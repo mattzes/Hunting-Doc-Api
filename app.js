978aee9a84a9668ba0902691892df372339fc6e7
KRPiefke <matteswitti@gmail.com>
2021-05-19 23:07:01 +0200

    Add DB connection handling

--- app.js
+++ app.js
@@ -21,11 +21,38 @@ app.use('/api/shooting', shootingRoute);
 app.use(clientError);
 
 // * Connect to DB
-mongoose.connect(
-  process.env.DB_CONNECTION,
-  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
-  () => console.log('DB connected!')
-);
+const db = mongoose.connection;
+
+db.on('connecting', function () {
+  console.log('connecting to MongoDB...');
+});
+db.on('error', function (error) {
+  console.error('Error in MongoDb connection: ' + error);
+  mongoose.disconnect();
+});
+db.on('connected', function () {
+  console.log('MongoDB connected!');
+});
+db.once('open', function () {
+  console.log('MongoDB connection opened!');
+});
+db.on('reconnected', function () {
+  console.log('MongoDB reconnected!');
+});
+db.on('disconnected', function () {
+  console.log('MongoDB disconnected!');
+  mongoose.connect(process.env.DB_CONNECTION_DEV, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  });
+});
+
+mongoose.connect(process.env.DB_CONNECTION_DEV, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 // * START APP ON PORT 3000
 app.listen(process.env.PORT, () => console.log('Server Up and running on Port: ' + process.env.PORT));
