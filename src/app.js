7eb18e90e29059389f967da6c8425bcb9a484ce7
KRPiefke <matteswitti@gmail.com>
2022-02-27 20:27:39 +0100

    remove urlencoded middleware

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/api/status', statusRoute);
 app.use('/api/auth', authRoute);
