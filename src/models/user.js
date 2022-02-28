c4f017fda9bdc36b8f9d84a523f2a049585145e8
KRPiefke <matteswitti@gmail.com>
2022-02-28 18:38:21 +0100

    add first name, last name and initials to user schema

--- src/models/user.js
+++ src/models/user.js
@@ -8,6 +8,22 @@ const UserSchema = mongoose.Schema(
       minLength: 6,
       maxLength: 32,
     },
+    firstName: {
+      type: String,
+      required: true,
+      maxLength: 32,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      maxLength: 32,
+    },
+    initials: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 2,
+    },
     password: {
       type: String,
       required: true,
