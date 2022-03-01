9e0e78638cb52fbd66c2df3bded35782aa3affc5
KRPiefke <matteswitti@gmail.com>
2022-03-02 00:18:06 +0100

    update user validations

--- src/validations/user.js
+++ src/validations/user.js
@@ -1,19 +1,22 @@
 const Joi = require('joi');
 
+const passwordPattern = new RegExp(
+  /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*!@$%^&(){}[\]:#;<>,.?\/~_+\-=|])(?:[[0-9]|[a-z]|[A-Z]|[*!@$%^&(){}[\]:#;<>,?\/~_+\-=|]){8,32}$/
+);
+const usernamePattern = new RegExp(/^(?!.*[_.-]{2})[0-9a-zA-Z_.-]+$/);
+
 // * Register Validation
 const registerValidation = data => {
   const schema = Joi.object({
-    username: Joi.string().min(6).max(32).alphanum().required(),
-    password: Joi.string()
-      .min(8)
-      .max(32)
-      .required()
-      .pattern(new RegExp(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[\]:#;<>,.?\/~_+\-=|]).{8,32}$/)),
-    email: Joi.string().min(6).required().email(),
-    scopes: Joi.array().items(Joi.string().max(16)).max(16),
-    firstName: Joi.string().min(2).max(32).alphanum().required(),
-    lastName: Joi.string().min(2).max(32).alphanum().required(),
-    initials: Joi.string().min(2).max(2).alphanum(),
+    username: Joi.string().min(6).max(32).required().pattern(usernamePattern),
+    password: Joi.string().min(8).max(32).required().pattern(passwordPattern),
+    email: Joi.string().required().email(),
+    scopes: Joi.object({
+      isAdmin: Joi.bool(),
+    }),
+    firstName: Joi.string().min(2).max(32).required(),
+    lastName: Joi.string().min(2).max(32).required(),
+    initials: Joi.string().min(2).max(2),
   });
   return schema.validate(data);
 };
@@ -21,14 +24,10 @@ const registerValidation = data => {
 // * Login Validation
 const loginValidation = data => {
   const schema = Joi.object({
-    username: Joi.string().min(6).max(32).alphanum().required(),
+    username: Joi.string().min(6).max(32).required().pattern(usernamePattern),
     refreshTokens: Joi.array().items(Joi.string()),
-    password: Joi.string()
-      .min(8)
-      .max(32)
-      .required()
-      .pattern(new RegExp(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[\]:#;<>,.?\/~_+\-=|]).{8,32}$/)),
-    rememberMe: Joi.boolean(),
+    password: Joi.string().min(8).max(32).required().pattern(passwordPattern),
+    rememberMe: Joi.bool(),
   });
   return schema.validate(data);
 };
